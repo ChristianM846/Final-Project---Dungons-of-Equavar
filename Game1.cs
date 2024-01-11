c0bd9e5bc3a3312eac2806673b77d79e5ffca74e
Christian Moyes <moyechri846@gmail.com>
2024-01-11 11:00:33 -0500

    Move on to character selection

--- Game1.cs
+++ Game1.cs
@@ -26,6 +26,7 @@ namespace Final_Project___Dungons_of_Equavar
         SpriteFont titleFont;
         SpriteFont introFont;
         SpriteFont statFont;
+        SpriteFont toSkipFont;
 
         SoundEffect introTheme;
         SoundEffectInstance introThemeInstance;
@@ -84,6 +85,7 @@ namespace Final_Project___Dungons_of_Equavar
             //Intro
             backgroundTexture = Content.Load<Texture2D>("rectangle");
             introFont = Content.Load<SpriteFont>("Intro");
+            toSkipFont = Content.Load<SpriteFont>("Tips");
             introTextY = _graphics.PreferredBackBufferHeight;
 
             // Battle
@@ -164,28 +166,45 @@ namespace Final_Project___Dungons_of_Equavar
             else if (screen == Screen.Intro)
             {
                 _spriteBatch.Draw(backgroundTexture, backgroundRect, Color.Black);
-                _spriteBatch.DrawString(introFont, "The thriving world of Equavar,", new Vector2(175, introTextY), Color.White);
+                _spriteBatch.DrawString(introFont, "The thriving world of Equavar,", new Vector2(185, introTextY), Color.White);
                 _spriteBatch.DrawString(introFont, "a world filled with magic, and", new Vector2(195, introTextY + 50), Color.White);
                 _spriteBatch.DrawString(introFont, "races of every kind,", new Vector2(280, introTextY + 100), Color.White);
                 _spriteBatch.DrawString(introFont, "the diverse humans, the elegant elves,", new Vector2(120, introTextY + 150), Color.White);
                 _spriteBatch.DrawString(introFont, "the fearsome dragonkin, the sturdy dwarves,", new Vector2(70, introTextY + 200), Color.White);
                 _spriteBatch.DrawString(introFont, "the raging orcs, the tricky demons,", new Vector2(170, introTextY + 250), Color.White);
-                _spriteBatch.DrawString(introFont, "and so many more", new Vector2(295, introTextY + 300), Color.White);
-                _spriteBatch.DrawString(introFont, "However, among the thriving and", new Vector2(175, introTextY + 450), Color.White);
-                _spriteBatch.DrawString(introFont, "peacful creatures, there are times", new Vector2(170, introTextY + 500), Color.White);
-                _spriteBatch.DrawString(introFont, "of war and conflict,", new Vector2(280, introTextY + 550), Color.White);
-                _spriteBatch.DrawString(introFont, "The ongoing war between the,", new Vector2(175, introTextY + 700), Color.White);
-                _spriteBatch.DrawString(introFont, "material plane and the nine hells", new Vector2(175, introTextY + 750), Color.White);
+                _spriteBatch.DrawString(introFont, "and so many more.", new Vector2(295, introTextY + 300), Color.White);
+                _spriteBatch.DrawString(introFont, "However, among the thriving and", new Vector2(160, introTextY + 450), Color.White);
+                _spriteBatch.DrawString(introFont, "peacful creatures, there are times", new Vector2(155, introTextY + 500), Color.White);
+                _spriteBatch.DrawString(introFont, "of war and conflict.", new Vector2(285, introTextY + 550), Color.White);
+                _spriteBatch.DrawString(introFont, "The ongoing war between the,", new Vector2(180, introTextY + 700), Color.White);
+                _spriteBatch.DrawString(introFont, "material plane and the nine hells", new Vector2(165, introTextY + 750), Color.White);
                 _spriteBatch.DrawString(introFont, "has raged for years, as the evil", new Vector2(175, introTextY + 800), Color.White);
-                _spriteBatch.DrawString(introFont, "queen of the hells, ", new Vector2(175, introTextY + 850), Color.White);
-                _spriteBatch.DrawString(introFont, "The thriving country of Equavar,", new Vector2(175, introTextY + 900), Color.White);
-                _spriteBatch.DrawString(introFont, "The thriving country of Equavar,", new Vector2(175, introTextY + 950), Color.White);
-                _spriteBatch.DrawString(introFont, "The thriving country of Equavar,", new Vector2(175, introTextY + 1000), Color.White);
+                _spriteBatch.DrawString(introFont, "queen of the hells, Sinestra,", new Vector2(200, introTextY + 850), Color.White);
+                _spriteBatch.DrawString(introFont, "strives for control of the surface.", new Vector2(165, introTextY + 900), Color.White);
+                _spriteBatch.DrawString(introFont, "Amidst the hordes of monsters, heroes stand,", new Vector2(65, introTextY + 1050), Color.White);
+                _spriteBatch.DrawString(introFont, "swinging sword, launching arrows,", new Vector2(170, introTextY + 1100), Color.White);
+                _spriteBatch.DrawString(introFont, "and casting spells to defend their homes.", new Vector2(100, introTextY + 1150), Color.White);
+                _spriteBatch.DrawString(introFont, "But the conflict rages on, Sinetra", new Vector2(165, introTextY + 1300), Color.White);
+                _spriteBatch.DrawString(introFont, "has created a new super weapon,", new Vector2(160, introTextY + 1350), Color.White);
+                _spriteBatch.DrawString(introFont, "an artifcat capable of draining,", new Vector2(185, introTextY + 1400), Color.White);
+                _spriteBatch.DrawString(introFont, "the natural magic within a warrior", new Vector2(165, introTextY + 1450), Color.White);
+                _spriteBatch.DrawString(introFont, "and transfer it to herself.", new Vector2(230, introTextY + 1500), Color.White);
+                _spriteBatch.DrawString(introFont, "In the test run of this new weapon,", new Vector2(165, introTextY + 1650), Color.White);
+                _spriteBatch.DrawString(introFont, "Sinstra has captured a small group", new Vector2(155, introTextY + 1700), Color.White);
+                _spriteBatch.DrawString(introFont, "of warriors, drained them of their magic", new Vector2(120, introTextY + 1750), Color.White);
+                _spriteBatch.DrawString(introFont, "and has left them to rot in the dungeons.", new Vector2(115, introTextY + 1800), Color.White);
+                _spriteBatch.DrawString(introFont, "However, with such an imperfect device,", new Vector2(95, introTextY + 1950), Color.White);
+                _spriteBatch.DrawString(introFont, "a small peice of the warriors' magic remains,", new Vector2(65, introTextY + 2000), Color.White);
+                _spriteBatch.DrawString(introFont, "giving them the perfect chance", new Vector2(175, introTextY + 2050), Color.White);
+                _spriteBatch.DrawString(introFont, "to fight their way out,", new Vector2(270, introTextY + 2100), Color.White);
+                _spriteBatch.DrawString(introFont, "and perhaps, destory the artifact", new Vector2(170, introTextY + 2150), Color.White);
+                _spriteBatch.DrawString(introFont, "before Sinestra can use it again.", new Vector2(170, introTextY + 2200), Color.White);
+                _spriteBatch.DrawString(toSkipFont, "Click to Skip", new Vector2(800, 600), Color.White);
 
             }
             else if (screen == Screen.CharacterSelect)
             {
-
+                
             }
             else if (screen == Screen.BeforeFirstBattle)
             {
